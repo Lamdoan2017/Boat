859cd01c0b69ed572e8d09a0f33702e0e00099e9
Lamdoan2017 <127533861+Lamdoan2017@users.noreply.github.com>
2023-04-02 23:02:27 -0700

    Blockout level 2
    create path for level two

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,16 +15,33 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate ()
     {
         Vector3 forwardStep = new Vector3(0, 0, 1);
+        
         rb.MovePosition(transform.position + forwardStep * Time.deltaTime * forwardforce);
+
+        //rb.AddRelativeForce(0,0, forwardforce* Time.deltaTime);
+        
+       /* if (Input.GetKey(KeyCode.W) )
+        {
+            transform.Translate(0, 0, forwardforce * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -forwardforce * Time.deltaTime);
+        }*/
+
+
         //rb.AddForce(0, 0, forwardforce* Time.deltaTime);
         int sideforce = Random.Range(a, b);
         if (Input.GetKey("d"))
         {
             rb.AddForce(sideforce* Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            transform.Rotate(0, sideforce * Time.deltaTime, 0);
         }
         if (Input.GetKey("a"))
         {
             rb.AddForce(-sideforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            transform.Rotate(0, -sideforce * Time.deltaTime, 0);
         }
     }
 }
